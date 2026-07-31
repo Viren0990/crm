b7de8c84647e815b15ce802c29c054e7f818f455
Viren0990 <veerendraverma106@gmail.com>
2026-07-31 17:35:47 +0530

    Fix prisma.config.ts studio type error

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from '@prisma/config'
 import 'dotenv/config'
 
 export default defineConfig({
-  earlyAccess: true,
   datasource: {
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL,
-  },
-  studio: {
     url: process.env.DIRECT_URL || process.env.DATABASE_URL,
   }
 })
